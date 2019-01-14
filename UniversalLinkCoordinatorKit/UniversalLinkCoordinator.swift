d54f3750a4461ca89cf323e8254c13676d0dea18
atsushi130 <atsushi_main@outlook.jp>
2019-01-14 18:50:14 +0900

    implement: UniversalLinkCoordinator protocol

--- UniversalLinkCoordinatorKit/UniversalLinkCoordinator.swift
+++ UniversalLinkCoordinatorKit/UniversalLinkCoordinator.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import CoordinatorKit
+
+public protocol UniversalLinkCoordinator: Coordinator where Route: UniversalLinkHandlable {
+    static var scheme: String { get }
+    func transitionIfNeeded(from universalLink: String)
+}
+
+public extension UniversalLinkCoordinator {
+    public func transitionIfNeeded(from universalLink: String) {
+        let scheme = Self.scheme
+        guard let route = Self.Route.handle(scheme: scheme, universalLink: universalLink) else { return }
+        self.transition(to: route)
+    }
+}
+
+public protocol UniversalLinkHandlable {
+    static func handle(scheme: String, universalLink: String) -> Self?
+}
