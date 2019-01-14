9c6870a4239567c893c267227932e4510000c888
atsushi130 <atsushi_main@outlook.jp>
2019-01-14 18:49:25 +0900

    add: new files

---
+++ UniversalLinkCoordinatorKit/Extractable.swift
@@ -0,0 +1,9 @@
+//
+//  Extractable.swift
+//  UniversalLinkCoordinatorKit
+//
+//  Created by Atsushi Miyake on 2019/01/14.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
---
+++ UniversalLinkCoordinatorKit/UniversalLinkCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  UniversalLinkCoordinator.swift
+//  UniversalLinkCoordinatorKit
+//
+//  Created by Atsushi Miyake on 2019/01/14.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
