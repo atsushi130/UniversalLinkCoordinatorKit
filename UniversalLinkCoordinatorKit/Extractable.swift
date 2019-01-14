0a0c95af1900bfc2e7641a9aa109a0adbcad3802
atsushi130 <atsushi_main@outlook.jp>
2019-01-14 18:49:42 +0900

    add: Extractable

--- UniversalLinkCoordinatorKit/Extractable.swift
+++ UniversalLinkCoordinatorKit/Extractable.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+public protocol Extractable {
+    static func extract(from: String) -> Self?
+}
+
+public extension Int: Extractable {
+    public static func extract(from string: String) -> Int? {
+        return Int(string)
+    }
+}
+
+public extension String: Extractable {
+    public static func extract(from string: String) -> String? {
+        return string
+    }
+}
