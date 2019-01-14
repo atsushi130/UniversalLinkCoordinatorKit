1c5008f293cb87c9715780503a100120a223895f
atsushi130 <atsushi_main@outlook.jp>
2019-01-14 16:01:11 +0900

    first commit:tada:

---
+++ UniversalLinkCoordinatorKit/UniversalLinkCoordinatorKit.h
@@ -0,0 +1,19 @@
+//
+//  UniversalLinkCoordinatorKit.h
+//  UniversalLinkCoordinatorKit
+//
+//  Created by Atsushi Miyake on 2019/01/14.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UniversalLinkCoordinatorKit.
+FOUNDATION_EXPORT double UniversalLinkCoordinatorKitVersionNumber;
+
+//! Project version string for UniversalLinkCoordinatorKit.
+FOUNDATION_EXPORT const unsigned char UniversalLinkCoordinatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniversalLinkCoordinatorKit/PublicHeader.h>
+
+
