0ef16d8e972b490adefe2d75d3a23cb5eaee7e97
atsushi130 <atsushi_main@outlook.jp>
2019-01-22 00:50:59 +0900

    reimplmenent: UniversalLInkCaseIterable

---
+++ UniversalLinkCoordinatorKit/UniversalLinkCaseIterable.swift
@@ -0,0 +1,29 @@
+//
+//  UniversalLinkCaseIterable.swift
+//  UniversalLinkCoordinatorKit
+//
+//  Created by Atsushi Miyake on 2019/01/22.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+public protocol UniversalLinkCaseIterable {
+    static var allCases: [Self] { get }
+}
+
+public extension UniversalLinkCaseIterable where Self: Hashable {
+    
+    private static var iterator: AnyIterator<Self> {
+        var n = 0
+        return AnyIterator {
+            defer { n += 1 }
+            let next = withUnsafePointer(to: &n) {
+                UnsafeRawPointer($0).assumingMemoryBound(to: Self.self).pointee
+            }
+            return next.hashValue == n ? next : nil
+        }
+    }
+    
+    public static var allCases: [Self] {
+        return Array(self.iterator)
+    }
+}
