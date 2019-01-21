bb86a7f8b13cdb9e0117b0484189960d9a9007c7
atsushi130 <atsushi_main@outlook.jp>
2019-01-21 23:18:00 +0900

    implement: UniversalLinkRouter

--- UniversalLinkCoordinatorKit/UniversalLinkRouter.swift
+++ UniversalLinkCoordinatorKit/UniversalLinkRouter.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+public final class UniversalLinkRouter<UniversalLink> where UniversalLink: UniversalLinkable {
+
+    private var routes: [UniversalLink] = []
+    
+    public func register(routes: [UniversalLink]) {
+        routes.forEach { self.routes.append($0) }
+    }
+    
+    public func handle(_ universalLink: URL) -> (UniversalLink, UniversalLinkContext)? {
+        return self.routes
+            .lazy
+            .flatMap { route in
+                if let context = route.parse(universalLink) {
+                    return (route, context)
+                } else {
+                    return nil
+                }
+            }
+            .first
+    }
+}
