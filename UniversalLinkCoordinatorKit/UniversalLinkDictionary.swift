ea7223dfc9e824bb5e60470884c19eec3ed38ba0
atsushi130 <atsushi_main@outlook.jp>
2019-01-21 23:17:22 +0900

    implement: UniversalLinkDictionary

--- UniversalLinkCoordinatorKit/UniversalLinkDictionary.swift
+++ UniversalLinkCoordinatorKit/UniversalLinkDictionary.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+public struct UniversalLinkDictionary {
+    
+    private let dictionary: [String: String]
+    
+    public init(_ dictionary: [String: String]) {
+        self.dictionary = dictionary
+    }
+    
+    public subscript<T: Extractable>(key: String, as type: T.Type) -> T? {
+        guard let value = self.dictionary[key] else { return nil }
+        return type.extract(from: value)
+    }
+    
+    public subscript(key: String) -> String? {
+        return self.dictionary[key]
+    }
+}
