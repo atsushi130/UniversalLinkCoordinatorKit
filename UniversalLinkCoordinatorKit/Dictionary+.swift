b8194eb68c3de0c8dc04a9b37183d44b897bafc9
atsushi130 <atsushi_main@outlook.jp>
2019-01-21 23:16:56 +0900

    implement: Dictionary + operator overload

--- UniversalLinkCoordinatorKit/Dictionary+.swift
+++ UniversalLinkCoordinatorKit/Dictionary+.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+extension Dictionary {
+    static func + (lhs: Dictionary, rhs: Dictionary) -> Dictionary {
+        var dictionary = lhs
+        rhs.forEach { element in
+            dictionary[element.key] = element.value
+        }
+        return dictionary
+    }
+}
