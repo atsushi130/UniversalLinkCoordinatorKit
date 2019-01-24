80cb2208354fdf1cd54f384cf4671b0cad09f233
atsushi130 <atsushi_main@outlook.jp>
2019-01-24 22:13:21 +0900

    add: discardableResult annotation

--- UniversalLinkCoordinatorKit/UniversalLinkCoordinator.swift
+++ UniversalLinkCoordinatorKit/UniversalLinkCoordinator.swift
@@ -12,11 +12,13 @@ public protocol UniversalLinkCoordinator: class {
     associatedtype Route
     associatedtype UniversalLink: UniversalLinkable
     var router: UniversalLinkRouter<UniversalLink> { get }
+    @discardableResult
     func transitionIfPossible(open universalLink: URL) -> Bool
     func transition(to route: Route)
 }
 
 public extension UniversalLinkCoordinator where Route == (universalLink: UniversalLink, context: UniversalLinkContext) {
+    @discardableResult
     public func transitionIfPossible(open universalLink: URL) -> Bool {
         guard let route = self.router.handle(universalLink) else { return false }
         self.transition(to: route)
