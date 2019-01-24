e3cfebd96e94c30a5b1a42a4f17003667b9f2a49
atsushi130 <atsushi_main@outlook.jp>
2019-01-24 22:32:17 +0900

    add: userInfo and options + implement configureInformation method

--- UniversalLinkCoordinatorKit/UniversalLinkContext.swift
+++ UniversalLinkCoordinatorKit/UniversalLinkContext.swift
@@ -6,15 +6,27 @@
 //  Copyright © 2019年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public struct UniversalLinkContext {
     
     public let parameters: UniversalLinkDictionary
     public let queryString: UniversalLinkDictionary
+    public private(set) var userInfo: [AnyHashable : Any] = [:]
+    public private(set) var options: [UIApplicationOpenURLOptionsKey: Any] = [:]
     
     public init(_ parameters: [String: String], and queryString: [String: String]) {
         self.parameters  = UniversalLinkDictionary(parameters)
         self.queryString = UniversalLinkDictionary(queryString)
+    }
+    
+    mutating func configureInformation(_ information: UniversalLinkInformation?) {
+        guard let information = information else { return }
+        switch information {
+        case .options(let options):
+            self.options = options
+        case .userInfo(let userInfo):
+            self.userInfo = userInfo
+        }
     }
 }
