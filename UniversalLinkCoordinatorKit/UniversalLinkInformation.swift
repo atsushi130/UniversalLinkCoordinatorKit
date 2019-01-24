467aeae446dc29a32e9e04c369240ce725f53a7c
atsushi130 <atsushi_main@outlook.jp>
2019-01-24 22:31:02 +0900

    add: UniversalLinkInformation file

---
+++ UniversalLinkCoordinatorKit/UniversalLinkInformation.swift
@@ -0,0 +1,9 @@
+//
+//  UniversalLinkInformation.swift
+//  UniversalLinkCoordinatorKit
+//
+//  Created by Atsushi Miyake on 2019/01/24.
+//  Copyright © 2019年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
